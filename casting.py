a908054966e6bc718075f9a21039b5d493323d54
manelHowlin1 <mhowlin@mail.austral.edu.ar>
2026-03-16 20:59:24 -0300

    Update casting.py

--- casting.py
+++ casting.py
@@ -1,3 +1,14 @@
 def casting():
     """Lee precio, descuento y cantidad como texto y calcula el precio con descuento y el total."""
+    precio = int(input())
+    descuento = float(input())
+    cantidad =int(input())
+    precio_descuento = (precio-descuento)
+    print(f"Precio: {precio}")
+    print(f"Descuento: {descuento}")
+
+    print(f"Precio con descuento: {precio_descuento}")
+
+    total = precio_descuento * cantidad
+    print("Total:",total)
     pass
