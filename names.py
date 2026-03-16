3ec71325cac1baac35a3546a6bbf263c733652d3
manelHowlin1 <mhowlin@mail.austral.edu.ar>
2026-03-16 20:08:44 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,16 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
+    nombre = input()
+    apellido = input()
+
+    nombre_minusculas = nombre.lower() + " " + apellido.lower()
+    nombre_titulo = nombre.title() + " " + apellido.title()
+    nombre_mayusculas = nombre.upper() + " " + apellido.upper()
+
+    print(nombre_minusculas)
+    print(nombre_titulo)
+    print(nombre_mayusculas)
+    print(f"\t{nombre_minusculas}")
+
     pass
