5fbdfe96b63222524c9001e94c725623d18434f3
manelHowlin1 <mhowlin@mail.austral.edu.ar>
2026-03-17 10:49:17 -0300

    Update string_methods.py

--- string_methods.py
+++ string_methods.py
@@ -8,3 +8,19 @@ def string_methods():
     multilinea = """Linea 1
     Linea 2
     Linea 3"""
+    print(f"Strip: {nombre.strip()}")
+    print(f"Lstrip: {nombre.lstrip()}")
+    print(f"Rstrip: {nombre.rstrip()}")
+    print(f"Upper: {frase.upper()}")
+    print(f"Lower: {frase.lower()}")
+    print(f"Title: {frase.title()}")
+    print(f"Find: {frase.find('gran')}")
+    print(f"Replace: {frase.replace('programacion','desarrollo')}")
+    print(f"Count: {frase.count('a')}")
+    print(f"Contiene Python: {'Python' in frase }")
+    print(f"Contiene Java: {'Java' in frase}")
+    print(f"Slice: {frase[:6]}")
+    print(f"Paso: {frase[:6:2]}")
+    print(f"Reverso:{frase[6::-1]}")
+    print(f"Formato: {nombre.strip()} sabe Python")
+    print(f"{multilinea[:7]}\n{multilinea[8:20].lstrip()}{multilinea[24:]}")
